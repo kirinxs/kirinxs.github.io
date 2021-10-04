5444b768192691c472f8fceab913cb45a8e77349
xmvp <88470725+xmvp@users.noreply.github.com>
2021-10-04 08:22:26 +0800

    refresh

--- games/src/Editor/SerializerUtils/index.js
+++
@@ -1,5 +0,0 @@
-import { RectangleCoverage } from './RectangleCoverage'
-
-export function makeRectangleCollection (width, height, map) {
-  return new RectangleCoverage(width, height, map).solve()
-}
--- games/webpack.editor-config.js
+++
@@ -1,5 +0,0 @@
-const config = require('./webpack.config')
-
-config.entry.app = './src/editorEntry.js'
-
-module.exports = config
